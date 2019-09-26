abe6b06ac6297add6ebe95ca4bf7a199f20709b3
Brittany Javalera <brittjavalera@gmail.com>
2019-09-26 22:54:15 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class OwnersController < ApplicationController
 
   get '/owners' do
@@ -5,25 +6,37 @@ class OwnersController < ApplicationController
     erb :'/owners/index' 
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
-    @owner = Owner.find(params[:id])
+    @owner = Owner.find_by_id(params[:id])
     erb :'/owners/edit'
   end
 
   get '/owners/:id' do 
-    @owner = Owner.find(params[:id])
+    @owner = Owner.find_by_id(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    @owner = Owner.find_by_id(params[:id])
+    @owner.update(params[:owner])
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    @owner.save
+    redirect "/owners/#{@owner.id}"
   end
 end
